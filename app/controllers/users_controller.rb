1daee537d748a557d2d3b445bf94bde42efed5b0
伊東和成 <itokazunari5375@icloud.com>
2021-11-14 15:11:28 +0900

    create signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  # def new
-  #   @user = User.new
-  # end
+  def new
+    @user = User.new
+  end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存時の成功をここで扱う
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
