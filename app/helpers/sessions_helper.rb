5250c063e5854564a542c86c2e450768d9941042
伊東和成 <itokazunari5375@icloud.com>
2021-11-20 12:22:47 +0900

    create login function

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,20 @@
 module SessionsHelper
+
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # 現在ログイン中のユーザーを返す（いる場合）
+  def current_user
+    if @current_user.nil?
+      @current_user = User.find_by(id: session[:user_id])
+    else
+      @current_user
+    end
+  end
+  # ログイン判定
+  def logged_in?
+    !current_user.nil?
+  end
 end
